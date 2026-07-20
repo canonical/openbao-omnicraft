9ee0d777f71d2dd87564b99e7da99cd8ef2eba19
kayra1 <kayra.gemalmaz@canonical.com>
2026-07-21 00:32:26 +0300

    fix rock

--- rock/2.5.2/tests/openbao.py
+++ rock/2.5.2/tests/openbao.py
@@ -2,7 +2,7 @@
 # Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-"""Contains all the specificities to communicate with Vault through its API."""
+"""Contains all the specificities to communicate with OpenBao through its API."""
 
 import logging
 from typing import List, Tuple
@@ -13,17 +13,17 @@ import requests  # type: ignore[import]
 logger = logging.getLogger(__name__)
 
 
-class Vault:
-    """Class to interact with Vault through its API."""
+class OpenBao:
+    """Class to interact with OpenBao through its API."""
 
     def __init__(self, url: str):
-        """Initialize Vault CLI client."""
+        """Initialize OpenBao CLI client."""
         self._client = hvac.Client(url=url, verify=False)
 
     def initialize(
         self, secret_shares: int = 1, secret_threshold: int = 1
     ) -> Tuple[str, List[str]]:
-        """Initialize Vault.
+        """Initialize OpenBao.
 
         Returns:
             A tuple containing the root token and the unseal keys.
@@ -31,25 +31,25 @@ class Vault:
         initialize_response = self._client.sys.initialize(
             secret_shares=secret_shares, secret_threshold=secret_threshold
         )
-        logger.info("Vault is initialized")
+        logger.info("OpenBao is initialized")
         return initialize_response["root_token"], initialize_response["keys"]
 
     def is_initialized(self) -> bool:
-        """Return whether Vault is initialized."""
+        """Return whether OpenBao is initialized."""
         return self._client.sys.is_initialized()
 
     def is_sealed(self) -> bool:
-        """Return whether Vault is sealed."""
+        """Return whether OpenBao is sealed."""
         return self._client.sys.is_sealed()
 
     def unseal(self, unseal_keys: List[str]) -> None:
-        """Unseal Vault."""
+        """Unseal OpenBao."""
         for unseal_key in unseal_keys:
             self._client.sys.submit_unseal_key(unseal_key)
-        logger.info("Vault is unsealed")
+        logger.info("OpenBao is unsealed")
 
     def is_api_available(self) -> bool:
-        """Return whether Vault is available."""
+        """Return whether OpenBao is available."""
         self._client.sys.read_health_status()
         try:
             self._client.sys.read_health_status()
@@ -58,5 +58,5 @@ class Vault:
         return True
 
     def set_token(self, token: str) -> None:
-        """Set the Vault token for authentication."""
+        """Set the OpenBao token for authentication."""
         self._client.token = token
--- rock/2.5.2/tests/test_integration.py
+++ rock/2.5.2/tests/test_integration.py
@@ -2,7 +2,7 @@
 # Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-"""Integration tests to validate that the Vault ROCK works as expected."""
+"""Integration tests to validate that the OpenBao ROCK works as expected."""
 
 import logging
 import subprocess
@@ -11,31 +11,31 @@ import time
 import pytest
 import requests
 
-from vault import Vault
+from openbao import OpenBao
 
 logger = logging.getLogger(__name__)
 
 
 def wait_for_vault_to_be_available(timeout: int = 30) -> None:
-    """Wait for Vault to be available."""
+    """Wait for OpenBao to be available."""
     initial_time = time.time()
-    vault = Vault("http://localhost:8200")
+    vault = OpenBao("http://localhost:8200")
     while time.time() - initial_time < timeout:
         try:
             if vault.is_api_available():
-                logger.info("Vault API is available")
+                logger.info("OpenBao API is available")
                 return
         except Exception:
             pass
         time.sleep(1)
-    raise TimeoutError("Vault is not available after {} seconds".format(timeout))
+    raise TimeoutError("OpenBao is not available after {} seconds".format(timeout))
 
 
 @pytest.fixture(scope="module")
 def run_container():
-    """Start a Vault container."""
+    """Start a OpenBao container."""
     subprocess.check_call(
-        "docker run -d -p 8200:8200 vault-rock:test",
+        "docker run -d -p 8200:8200 openbao-rock:test",
         shell=True,
     )
 
@@ -46,15 +46,15 @@ def test_given_vault_container_running_when_ui_is_enabled_then_ui_is_available(r
     response = requests.get("http://localhost:8200/ui/vault/init")
 
     assert response.status_code == 200
-    assert "Vault UI is not available in this binary." not in response.text  # This is the message when UI is disabled  # noqa: E501
+    assert "OpenBao UI is not available in this binary." not in response.text  # This is the message when UI is disabled  # noqa: E501
     assert "vault/config/environment" in response.text  # This is a common element in the UI
 
 
 def test_given_vault_container_running_when_initialize_then_properly_responds_to_commands(
     run_container
 ):
-    """Runs basic CLI commands to initialize and unseal Vault."""
-    vault = Vault("http://localhost:8200")
+    """Runs basic CLI commands to initialize and unseal OpenBao."""
+    vault = OpenBao("http://localhost:8200")
 
     wait_for_vault_to_be_available()
 
